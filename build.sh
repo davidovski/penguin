59a76335613b836312f03a539e61c00a09b0962d
davidovski <david@davidovski.xyz>
2023-10-17 03:26:58 +0100

    initial commit

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+name=pengui
+
+rm -rf build
+mkdir -p build
+
+cd build
+env GOOS=js GOARCH=wasm go build -o $name.wasm ..
+cp $(go env GOROOT)/misc/wasm/wasm_exec.js .
+cp ../index.html .
+
+[ "$1" = "serve" ] && gopherjs serve
+
